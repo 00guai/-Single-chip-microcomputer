150f06f55acba4aedb423d322ba1631b2fcda3cc
00guai <1986055395@qq.com>
2021-12-15 23:05:23 +0800

    Relay

---
+++ 继电器/main.c
@@ -0,0 +1,20 @@
+#include<reg52.h>
+#iclude<intrins.h>
+
+typedef unsigned int u 16;
+typedef unsigned int char u 8;
+
+
+sbit relay=P1^4;
+
+
+
+void main(u 16)
+{
+	relay=0;
+	while(1)
+	{
+		
+	}
+	
+}
